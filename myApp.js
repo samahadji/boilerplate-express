830e8795e510191ff9aa6d54b6810f0c00a8f055
Samson Ahadji <samahadji@gmail.com>
2022-06-15 23:01:16 +0000

    Fixing the express.static path again

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 
 let publicPath = __dirname + "/public"
-app.use(publicPath, express.static(publicPath));
+app.use("/public", express.static(publicPath));
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + '/views/index.html')
