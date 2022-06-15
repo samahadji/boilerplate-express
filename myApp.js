6b9dd40a3a5099b4eefee81d4ff0d51f94e5544b
Samson Ahadji <samahadji@gmail.com>
2022-06-15 22:21:56 +0000

    Adding the public static folder

--- myApp.js
+++ myApp.js
@@ -1,7 +1,8 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World");
+app.use(express.static());
+express.static(__dirname + "/public");
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + '/views/index.html')
