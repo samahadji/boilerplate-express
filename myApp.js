8d5c4f7b50bdb33343a17cebf997c48ea023f717
Samson Ahadji <samahadji@gmail.com>
2022-06-15 22:26:29 +0000

    Fixing errors in the express.static

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
 let express = require('express');
 let app = express();
 
-app.use(express.static());
-express.static(__dirname + "/public");
+app.use(express.static(__dirname + "/public"));
+;
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + '/views/index.html')
