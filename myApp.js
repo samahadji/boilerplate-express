2d4bddb552e563f78667c065931f55357c085226
Samson Ahadji <samahadji@gmail.com>
2022-06-16 12:21:37 +0000

    Adding a middleware

--- myApp.js
+++ myApp.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
+
 let express = require('express');
 let app = express();
 
 let publicPath = __dirname + "/public"
 app.use("/public", express.static(publicPath));
 
+//Middlewares
+app.use((req, res, next) => {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+})
+
+// Routes
 app.get("/", function(req, res) {
     res.sendFile(__dirname + '/views/index.html')
 })
