67ee082c0682e51c23fdcd080610d4ff45955313
Samson Ahadji <samahadji@gmail.com>
2022-06-16 13:46:41 +0000

    Adding POST handling

--- myApp.js
+++ myApp.js
@@ -45,6 +45,8 @@ app.get("/:word/echo", (req, res) => {
 
 app.route("/name").get((req, res) => {
     res.json({"name": req.query.first + " " + req.query.last})
+}).post((req, res) => {
+    res.json({"name": req.body.first + " " + req.body.last})
 })
 
 
